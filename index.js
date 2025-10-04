87db4b19621d31d46eced3e31986bdc7b655046c
amitsengupta <amitsengupta332@gmail.com>
2025-10-04 23:25:52 +0600

    check

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Percel Delivery API is running...");
+});
+
+ 
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
